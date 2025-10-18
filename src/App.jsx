4d07dff3528ad70503b265843fb35b58b3b78d76
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-10-18 19:24:00 +0600

    changed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-6xl">Let's Build Dragon News</h1>
+      <h1 className="text-50xl">Let's Build Dragon News</h1>
     </>
   );
 }
